17bd6da161dbec0fc294480d68f990467df43b23
Dave Enyi <29585292+Dave-Sleek@users.noreply.github.com>
2022-02-06 12:24:08 +0100

    footer

---
+++ footer.php
@@ -0,0 +1,10 @@
+<br><br><br><br>
+<footer class="mt-auto py-3 bg-dark">
+  <div class="container">
+    <div class="col-12 col-md" align="center">
+        <!-- <img class="mb-2" src="../assets/brand/bootstrap-logo.svg" alt="" width="24" height="19"> -->
+        <span class="text-muted"><img src="assets/img/LM2.png" height="150" width="150" class="img-fluid mx-auto d-block" alt="Image coming soon"></span>
+        <small class="d-block mb-3 text-muted"><span class="text-muted">Powered By: GetBusyCoding &copy; <?php echo date("Y"); ?></span></small>
+      </div>
+  </div>
+  </footer>
